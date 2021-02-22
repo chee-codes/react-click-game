c022bde633beb24e3ee33f5b3de5c1ef7ed93f98
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-02-22 01:10:10 -0600

    Update compontents folder

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "./components/Nav/index";
 import Header from "./components/Header/index";
+import ImgCard from "./components/ImageCard";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
+      <ImgCard />
     </div>
   );
 }
---
+++ click-game/src/components/ImageCard/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Style from "./style.css";
+
+const ImgCard = () => {
+  return <h1>Dummy Text</h1>;
+};
+
+export default ImgCard;
